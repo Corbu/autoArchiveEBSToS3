9b9e04d9fcb740b81c2172a058da3a67d25dc171
Cory Burns <cburns@corp.safesecureweb.com>
2016-12-07 18:06:11 -0500

    Get EBS volumes from stopped EC2 instance

--- aa_cw.py
+++ aa_cw.py
@@ -1,12 +1,26 @@
- # aa_cw.py
- # CloudWatch functions
+# aa_cw.py
+# Functions regarding the JSON details from the CloudWatch event
  
 import boto3
 import json
 
 def getInstanceID(event):
+	"""Grabs the EC2 Instance ID from the CloudWatch JSON"""
 	return json.loads(json.dumps(event))['detail']['instance-id']
 	
+def returnEBS(id):
+	"""Outputs all the attached EBS volumes of an instance"""
+	ec2 = boto3.resource('ec2')
+	volume_id_list = []
+	
+	volumes = ec2.Instance(id).volumes.all()
+	
+	for v in volumes:
+		volume_id_list.append(v.id)
+		
+	return volume_id_list
+
 def lambda_handler(event, context):
-    id = getInstanceID(event)
-    return id
+	id = getInstanceID(event)
+	volumes = returnEBS(id)
+	return volumes
