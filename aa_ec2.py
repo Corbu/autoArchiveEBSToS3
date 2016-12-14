43337aa3016022e53da016d4ffeaa1f52ceab6e2
Cory Burns <cburns@corp.safesecureweb.com>
2016-12-14 12:52:51 -0500

    Added EC2 investigation functions, cleaned up ec2 create function

--- aa_ec2.py
+++ aa_ec2.py
@@ -1,38 +1,35 @@
-# autoArchive_ec2.py
-# Creates a new EC2 instance
-
-# AWS CLI:
-# aws ec2 run-instances --image-id ami-xxxxxxxx --count 1 --instance-type t2.micro --key-name MyKeyPair --security-group-ids sg-xxxxxxxx 
-	
-# This is from the boto3.readthedocs.io page.  I think I have this paired down to what I need, I just need to turn these into
-# variables that make sense...and figure out the EBS syntax specifically
-# http://boto3.readthedocs.io/en/latest/reference/services/ec2.html#EC2.Client.run_instances
+# aa_ec2.py
+# Functions regarding EC2 instance manipulation
 
 import boto3
 
-def createEC2(ami,availabilityZone):
+def createEC2(ami,availabilityZone,vpc,userData,iamRole):
+	"""Creates a temporary EC2 instance that mounts all EBS volumes from a stopped EC2 instance and stores them in S3
+	ami = Typically this will be the default AWS Linux AMI
+	availabilityZone = the availability zone the EC2 instance will be created in"""
+	
+	# This was mostly pulled from the BOTO3 docs page:
+	# http://boto3.readthedocs.io/en/latest/reference/services/ec2.html#EC2.Client.run_instances
 	client = boto3.client('ec2')
+	
 	response = client.run_instances(
-		ImageId=ami, # Required, this will be replaced with a variable
-		MinCount=1, # Required
-		MaxCount=1, # Required
-		# SecurityGroups=[ # This will be replaced with a variable
-		#	'string', # For Default VPCs
-		#],
-		#SecurityGroupIds=[
-		#	'string', # For non-default VPCs 
-		#],
+		ImageId=ami, # The AMI of the EC2 Instance
+		# We only want one instance created
+		MinCount=1,
+		MaxCount=1,
+		SecurityGroupIds=[vpc] # The VPC that this will run in
 		# The temp EC2 instance will have a bootstrap script that will:
 			## Unmount the EBS instances from the original EC2 instance
 			## Mount the EBS instances on the temp EC2 instance
 			## Copy data from the mounted EBS instances to the appropriate S3 bucket
 			## Re-mount EBS volumes to original EC2 instance
 			## Shutdown the server (which will terminate the instance as per the below settings)
-		# UserData='string', # This should also be replaced with a variable
-		InstanceType='t2.micro', # Required
+		UserData=userData,
+		InstanceType='t2.micro', # You can change this if neccessary, but a t2 micro should be sufficient
 		Placement={
-			'AvailabilityZone': availabilityZone
+			'AvailabilityZone': availabilityZone # The availability zone the EC2 Instance is going to be placed in
 		},
+		# We just need a small default EBS volume for the OS
 		BlockDeviceMappings=[
 			{
 				'DeviceName': '/dev/xvda',
@@ -52,3 +49,67 @@ def createEC2(ami,availabilityZone):
 		#	'Name': 'string'
 		#},
 	)
+
+def returnEBS(id, root):
+	"""Outputs all the attached EBS volumes of an instance
+	id = the Instance ID
+	root = include the root volume.  0 no, 1 yes.  Defaults to yes"""
+	ec2 = boto3.resource('ec2')
+	volume_id_list = []
+	
+	volumes = ec2.Instance(id).volumes.all()
+	
+	if root==0:
+		block_devices = ec2.Instance(id).block_device_mappings
+		
+		# Get the root volume ID
+		for b in block_devices:
+			if b['DeviceName'] == ec2.Instance(id).root_device_name:
+				root_volume = b['Ebs']['VolumeId']
+
+		# Get the list of EBS volumes attached, except for the root volume
+		for v in volumes:
+			if v.id != root_volume:
+				volume_id_list.append(v.id)
+	else:
+		for v in volumes:
+			volume_id_list.append(v.id)
+		
+	return volume_id_list
+
+def returnAZ(id):
+	"""Returns the Availability Zone of an EC2 instance
+	id = Instance ID"""
+	
+	ec2 = boto3.resource('ec2')
+	return ec2.Instance(id).placement['AvailabilityZone']
+
+def returnOS(id):
+	"""Returns the OS of an EC2 instance.
+	id = Instance ID"""
+	
+	ec2 = boto3.resource('ec2')
+	
+	# Platform only returns Windows or blank
+	if ec2.Instance(id).platform != 'Windows':
+		return 'Linux'
+	else:
+		return ec2.Instance(id).platform
+
+def returnVPC(id):
+	"""Returns the VPC of an EC2 instance
+	id = Instance ID"""
+	
+	ec2 = boto3.resource('ec2')
+	return ec2.Instance(id).vpc_id
+
+
+def lambda_handler(event, context):
+	id = getInstanceID(event)
+	volumes = returnEBS(id, '1')
+	az = returnAZ(id)
+	os = returnOS(id)
+	vpc = returnVPC(id)
+	
+	return volumes, az, os, vpc
+
